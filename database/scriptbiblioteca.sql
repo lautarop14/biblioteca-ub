6a1899f623261f5a8adcc25011368fcbde7baf67
Karolsito <karolsilva@live.com.ar>
2026-02-02 20:49:12 -0300

    Update scriptbiblioteca.sql

--- database/scriptbiblioteca.sql
+++ database/scriptbiblioteca.sql
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS prestamos (
     fecha_devolucion_real DATE,
     estado ENUM('activo', 'devuelto') NOT NULL DEFAULT 'activo',
     FOREIGN KEY (libro_id) REFERENCES libros(id) ON DELETE CASCADE,
-    FOREIGN KEY (usuario_id) REFERENCES bibliotecarios(id)
+    FOREIGN KEY (usuario_id) REFERENCES bibliotecarios(id) ON DELETE CASCADE
 );
 
 INSERT INTO bibliotecarios (usuario, password_hash, nombre_completo, admin)
